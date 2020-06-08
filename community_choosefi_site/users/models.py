6b6d6f28f4489f608bfa1e94d87af87e673d8ec6
William McVey <wam@wamber.net>
2020-06-07 23:47:27 -0400

    Initial Profile Info

--- community_choosefi_site/users/models.py
+++ community_choosefi_site/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
@@ -8,7 +8,23 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    location = models.TextField(blank=True,
+                                help_text="Your approximate address",
+                                verbose_name="Address")
+    share_location = models.BooleanField(default=False,
+                                         help_text="Share location with other members")
+    share_resolution = models.IntegerField(
+        default=0,
+        help_text="Amount of resolution to apply to address. 0=exact. 5=appx 100km/60miles")
+
+    local_groups = models.ManyToManyField('choosefi_local.LocalGroupPage')
+    topic_groups = models.ManyToManyField('choosefi_local.TopicGroupPage')
+
+
+    def save_test(self, *args, **kwargs):
+        # See coderedcms/page_models.CoderedLocationPage.save()
+        super().save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
